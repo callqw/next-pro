f128efcaf3e8ae27bd1ac61920cd63121dca21ce
callqw <402555290@qq.com>
2024-12-06 14:46:39 +0800

    middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,22 @@
-import { NextResponse, NextRequest } from 'next/server'
+import { NextResponse, NextRequest } from "next/server";
 
 // This function can be marked `async` if using `await` inside
 export function middleware(req: NextRequest) {
-    console.log(req.nextUrl.pathname, 'in about111');
+//   console.log(req.nextUrl.pathname, "in about111");
+  const validataLogin = req.nextUrl;
+//   console.log(validataLogin,'vaa');
+  
+  if (validataLogin.pathname === "/dashboard") {
+    console.log(validataLogin.pathname,"dashboard");
+    
+  } else {
 
-    // return NextResponse.redirect(new URL('/home', request.url))
+    console.log(validataLogin.pathname,"els11");
+  }
+  // return NextResponse.redirect(new URL('/home', request.url))
 }
 
 export const config = {
-    // matcher: '/',
-    matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'] //过滤的条件不显示
-}
+  // matcher: '/',
+  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"], //过滤的条件不显示
+};
