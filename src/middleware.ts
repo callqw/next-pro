753a35571280682efc6442dd4e97995e97d864f0
DESKTOP-SD6KS6B\admin <402555290@qq.com>
2024-12-06 10:24:00 +0800

    no message

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,13 @@
 import { NextResponse, NextRequest } from 'next/server'
 
 // This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
-    console.log('in about111');
+export function middleware(req: NextRequest) {
+    console.log(req.nextUrl.pathname, 'in about111');
 
     // return NextResponse.redirect(new URL('/home', request.url))
 }
 
 export const config = {
     // matcher: '/',
+    matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'] //过滤的条件不显示
 }
