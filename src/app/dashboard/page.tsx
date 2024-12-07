c98fcb29f109d687f8231035350965eceb15da43
callqw <402555290@qq.com>
2024-12-07 20:35:54 +0800

    ts类型，索引，映射。keyof typeof  in  number  length omit infer exgents 条件类型

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,6 +5,30 @@ const my: Global.MyGlobal = function (x) {
 const cock: Global.CockProps<number, boolean> = function (x) {
   return false
 }
+type Str = ['p1', "p2", "p3", "p4", "p5", "p6"];
+type Arr<U, T extends string[] = []> = T["length"] extends U ? T[number] : Arr<U, [...T, `p${T["length"]}`]>;
+
+type ObjOthers<U extends number> = {
+  [key in Arr<U>]: string;
+}
+type Obj = Omit<ObjOthers<7> & {
+  id: number
+}, "p0">
+type Fun<T extends string[] = []> = T["length"] extends 3 ? T[number] : Fun<[...T, `Su${T["length"]}`]>
+type Cc = Fun
+const obj: Obj = {
+  id: 1,
+  p1: "",
+  p2: "",
+  p3: "",
+  p4: "",
+  p5: "",
+  p6: "",
+}
+
+
+
+
 export default function Page() {
   let a = cock(12);
   console.log(typeof a);
