4e10e41c7e4cb3ad937ce2f9f3273391c18b1b8f
DESKTOP-SD6KS6B\admin <402555290@qq.com>
2024-12-09 22:35:59 +0800

    no message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,18 @@ import Link from "next/link";
 import { config } from "@/src/config";
 export const dynamic = 'force-dynamic'
 export default async function Home() {
-  const mainData = await fetch(`http://127.0.0.1:3000/api/cache`, { method: "GET" });
+  const mainData = await fetch(`http://127.0.0.1:3000/api/cache`, {
+    method: "GET", next: {
+      revalidate: 10
+    }
+  });
   const res = await mainData.json();
   const data: DefaultProps = { id: "12", name: '123' }
   return (
     <div>
       home
       {new Date().toLocaleString()}
-      <Image src={res.message} alt="asdf" width={50} height={50}></Image>
+      <Image src={res.message} alt="asdf" width={150} height={150}></Image>
     </div>
   );
 }
